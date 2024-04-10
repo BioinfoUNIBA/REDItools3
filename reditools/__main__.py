7b70116ebfdc0e4f89d20c634f2bea926460189b
Adam Handen <adam.handen@gmail.com>
2024-04-10 21:16:21 +0000

    tweaked commandline arguments

--- reditools/__main__.py
+++ reditools/__main__.py
@@ -1,6 +1,5 @@
 """Commandline tool for REDItools."""
 
-import argparse
 import sys
 
 from reditools import analyze, homopolymerics, index
@@ -8,31 +7,31 @@ from reditools import analyze, homopolymerics, index
 
 def usage():
     """Print program usage."""
-    parser = argparse.ArgumentParser(
-        description='REDItools 2.0',
-        prog='reditools',
-    )
-    parser.add_argument(
-        'command',
-        choices=[
-            'analyze',
-            'find-repeats',
-        ],
-        help='Either analyze or find-repeats.',
-    )
-    parser.parse_args()
+    print("""usage: reditools {analyze,find-repeats,index}
+
+REDItools3
+
+Run Modes:
+  analyze            Find editing events in one or more alignment files.
+
+  find-repeats       Find repetitive elements in a genome.
+
+  index              Calculate editing indices from the output of `analyze`
+                     mode.
+""")
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         command = sys.argv.pop(1)
-        if command == 'analyze':
-            analyze.main()
-        elif command == 'find-repeats':
-            homopolymerics.main()
-        elif command == 'index':
-            index.main()
-        else:
-            usage()
+        match command:
+            case 'analyze':
+                analyze.main()
+            case 'find-repeats':
+                homopolymerics.main()
+            case 'index':
+                index.main()
+            case _:
+                usage()
     else:
         usage()
