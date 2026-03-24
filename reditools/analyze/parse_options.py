a72cbe4fae3d61f27ea23a1328d576473178d72a
ahanden <adam.handen@gmail.com>
2026-03-24 12:34:30 -0500

    Added extended description of splice file

--- reditools/analyze/parse_options.py
+++ reditools/analyze/parse_options.py
@@ -1,7 +1,8 @@
 import argparse
+import sys
 
 
-def parse_options():  # noqa:WPS213,WPS432
+def parse_options():  # noqa:WPS213
     """
     Parse commandline options for REDItools.
 
@@ -16,20 +17,162 @@ def parse_options():  # noqa:WPS213,WPS432
     parser.add_argument(
         'file',
         nargs='+',
-        help='The bam file(s) to be analyzed.',
+        help='The BAM file(s) to be analyzed.',
     )
     parser.add_argument(
         '-r',
         '--reference',
-        help='Reference FASTA file.',
+        help='Reference FASTA file. (Note: REDItools runs fastest when ' +
+        'BAM files have MD tags and -r is *not* used).',
     )
     parser.add_argument(
+        '-N',
+        '--dna',
+        default=False,
+        help='Run REDItools on DNA-Seq data.',
+        action='store_true',
+    )
+    output_group = parser.add_argument_group(
+        title='Output Options',
+    )
+    output_group.add_argument(
         '-o',
         '--output-file',
         help='Path to write output to.',
         default='/dev/stdout',
     )
-    parser.add_argument(
+    output_group.add_argument(
+        '-a',
+        '--append-file',
+        action='store_true',
+        help='Appends results to file (and creates if not existing).',
+    )
+    bqf_group = parser.add_argument_group(
+        title='Base/Read Quality Controls',
+    )
+    bqf_group.add_argument(
+        '-mrl',
+        '--min-read-length',
+        type=int,
+        default=30,  # noqa:WPS432
+        help='Reads shorter than -mrl will be discarded.',
+    )
+    bqf_group.add_argument(
+        '-q',
+        '--min-read-quality',
+        type=int,
+        default=20,  # noqa:WPS432
+        help='Reads with MAPQ below -q will be discarded.',
+    )
+    bqf_group.add_argument(
+        '-bq',
+        '--min-base-quality',
+        type=int,
+        default=30,  # noqa:WPS432
+        help='Bases Phred quality score below -bq will bed discarded.',
+    )
+    bqf_group.add_argument(
+        '-mbp',
+        '--min-base-position',
+        type=int,
+        default=0,
+        help='Ignores the first -mbp bases in each read.',
+    )
+    bqf_group.add_argument(
+        '-Mbp',
+        '--max-base-position',
+        type=int,
+        default=0,
+        help='Ignores the last -Mpb bases in each read.',
+    )
+    bqf_group.add_argument(
+        '-E',
+        '--exclude-reads',
+        help='Text file listing read names to exclude from analysis.',
+    )
+    bqf_group.add_argument(
+        '--exclude_reads',
+        help=argparse.SUPPRESS,
+    )
+    gr_group = parser.add_argument_group(
+        title='Genomic Region Filters',
+    )
+    gr_group.add_argument(
+        '-k',
+        '--exclude-regions',
+        nargs='+',
+        help='Skip regions in the provided BED file(s).',
+    )
+    gr_group.add_argument(
+        '--exclude_regions',
+        help=argparse.SUPPRESS,
+    )
+    gr_group.add_argument(
+        '-m',
+        '--load-omopolymeric-file',
+        help='BED file of omopolymeric positions.',
+    )
+    gr_group.add_argument(
+        '-g',
+        '--region',
+        help='Only analyzes the specified region.',
+    )
+    gr_group.add_argument(
+        '-B',
+        '--bed-file',
+        nargs='+',
+        help='Only analyze regions in the provided BED file.',
+    )
+    gr_group.add_argument(
+        '--bed_file',
+        help=argparse.SUPPRESS,
+    )
+    rf_group = parser.add_argument_group(
+        title='Result Filters',
+    )
+    rf_group.add_argument(
+        '-l',
+        '--min-read-depth',
+        type=int,
+        default=1,
+        help='Only report on positions with at least -l read depth',
+    )
+    rf_group.add_argument(
+        '-men',
+        '--min-edits-per-nucleotide',
+        type=int,
+        default=0,
+        help='Positions with fewer than -men edits will be discarded.',
+    )
+    rf_group.add_argument(
+        '-me',
+        '--min-edits',
+        type=int,
+        default=1,
+        help='The minimum number of editing events (per position). ' +
+        'Positions with fewer than -me edits will be discarded.',
+    )
+    rf_group.add_argument(
+        '-Men',
+        '--max-editing-nucleotides',
+        type=int,
+        default=4,  # noqa:WPS432
+        help='The maximum number of editing nucleotides, from 0 to 4 ' +
+        '(per position). Positions whose columns have more than ' +
+        '"max-editing-nucleotides" will not be included in the analysis.',
+    )
+    rf_group.add_argument(
+        '-v',
+        '--variants',
+        nargs='*',
+        default=['all'],
+        help='Which editing events to report. Edits should be two characters, '
+        'separated by spaces. Use "all" to report all variants.',
+    )
+    strand_group = parser.add_argument_group(
+        title='Strandedness Options',
+    )
+    strand_group.add_argument(
         '-s',
         '--strand',
         choices=(0, 1, 2),
@@ -39,116 +182,15 @@ def parse_options():  # noqa:WPS213,WPS432
         '1 (second strand oriented) or ' +
         '2 (first strand oriented).',
     )
-    parser.add_argument(
-        '-a',
-        '--append-file',
-        action='store_true',
-        help='Appends results to file (and creates if not existing).',
-    )
-    parser.add_argument(
-        '-g',
-        '--region',
-        help='Only analyzes the specified region.',
-    )
-    parser.add_argument(
-        '-m',
-        '--load-omopolymeric-file',
-        help='BED file of omopolymeric positions.',
-    )
-    parser.add_argument(
-        '-sf',
-        '--splicing-file',
-        help='The file containing splicing site positions.',
-    )
-    parser.add_argument(
-        '-ss',
-        '--splicing-span',
-        type=int,
-        default=4,
-        help='The splicing span.',
-    )
-    parser.add_argument(
-        '-mrl',
-        '--min-read-length',
-        type=int,
-        default=30,
-        help='Reads with length below -mrl will be discarded.',
-    )
-    parser.add_argument(
-        '-q',
-        '--min-read-quality',
-        type=int,
-        default=20,
-        help='Reads with mapping quality below -q will be discarded.',
-    )
-    parser.add_argument(
-        '-bq',
-        '--min-base-quality',
-        type=int,
-        default=30,
-        help='Base quality below -bq will bed discarded.',
-    )
-    parser.add_argument(
-        '-mbp',
-        '--min-base-position',
-        type=int,
-        default=0,
-        help='Ignores the first -mbp bases in each read.',
-    )
-    parser.add_argument(
-        '-Mbp',
-        '--max-base-position',
-        type=int,
-        default=0,
-        help='Ignores the last -Mpb bases in each read.',
-    )
-    parser.add_argument(
-        '-l',
-        '--min-read-depth',
-        type=int,
-        default=1,
-        help='Only report on positions with at least -l read depth',
-    )
-    parser.add_argument(
-        '-e',
-        '--exclude-multis',
-        default=False,
-        help='Do not report any position with more than one alternate base.',
-        action='store_true',
-    )
-    parser.add_argument(
-        '-men',
-        '--min-edits-per-nucleotide',
-        type=int,
-        default=0,
-        help='Positions with fewer than -men edits will not be discarded.',
-    )
-    parser.add_argument(
-        '-me',
-        '--min-edits',
-        type=int,
-        default=1,
-        help='The minimum number of editing events (per position). ' +
-        'Positions with fewer than -me edits will be discarded.',
-    )
-    parser.add_argument(
-        '-Men',
-        '--max-editing-nucleotides',
-        type=int,
-        default=4,
-        help='The maximum number of editing nucleotides, from 0 to 3 ' +
-        '(per position). Positions whose columns have more than ' +
-        '"max-editing-nucleotides" will not be included in the analysis.',
-    )
-    parser.add_argument(
+    strand_group.add_argument(
         '-T',
         '--strand-confidence-threshold',
         type=float,
-        default=0.7,
+        default=0.7,  # noqa:WPS432
         help='Only report the strandedness if at least -T proportion of ' +
         'reads are of a given strand.',
     )
-    parser.add_argument(
+    strand_group.add_argument(
         '-C',
         '--strand-correction',
         default=False,
@@ -156,37 +198,17 @@ def parse_options():  # noqa:WPS213,WPS432
         'only bases according to this strand will be selected.',
         action='store_true',
     )
-    parser.add_argument(
-        '-V',
-        '--verbose',
-        default=False,
-        help='Run in verbose mode.',
-        action='store_true',
+    para_group = parser.add_argument_group(
+        title='Parallel Processing Options',
     )
-    parser.add_argument(
-        '-N',
-        '--dna',
-        default=False,
-        help='Run REDItools on DNA-Seq data.',
-        action='store_true',
-    )
-    parser.add_argument(
-        '-B',
-        '--bed_file',
-        nargs='+',
-        help='Only analyze regions in the provided BED file.',
-    )
-    parser.add_argument(
+    para_group.add_argument(
         '-t',
         '--threads',
         help='Number of threads for parallel processing.',
         type=int,
         default=1,
     )
-    parser.add_argument(
-        '--temp-dir',
-        help='Location to save temporary files')
-    parser.add_argument(
+    para_group.add_argument(
         '-w',
         '--window',
         help='How many bp should be processed by each thread at a time. ' +
@@ -194,31 +216,60 @@ def parse_options():  # noqa:WPS213,WPS432
         type=int,
         default=0,
     )
-    parser.add_argument(
-        '-k',
-        '--exclude_regions',
-        nargs='+',
-        help='Skip regions in the provided BED file(s).',
+    tech_group = parser.add_argument_group(
+        title='Technical Options',
     )
-    parser.add_argument(
-        '-E',
-        '--exclude_reads',
-        help='Text file listing read names to exclude from analysis.',
+    tech_group.add_argument(
+        '-V',
+        '--verbose',
+        default=False,
+        help='Run in verbose mode.',
+        action='store_true',
     )
-    parser.add_argument(
+    tech_group.add_argument(
         '-d',
         '--debug',
         default=False,
         help='Run in debug mode.',
         action='store_true',
     )
-    parser.add_argument(
-        '-v',
-        '--variants',
-        nargs='*',
-        default=['all'],
-        help='Which editing events to report. Edits should be two characters, '
-        'separated by spaces. Use "all" to report all variants.',
+    tech_group.add_argument(
+        '--temp-dir',
+        help='Location to save temporary files',
     )
+    leg_group = parser.add_argument_group(
+        title='Legacy Options',
+    )
+    leg_group.add_argument(
+        '-e',
+        '--exclude-multis',
+        default=False,
+        help=(
+            'Do not report any position with more than one alternate base. '
+            '(Equivalent to --max-editing-nucleotides 1)'
+        ),
+        action='store_true',
+    )
+
+    leg_group.add_argument(
+        '-sf',
+        '--splicing-file',
+        help=(
+            'The splicing file is a space delimited file with columns '
+            'chromosome, start position (zero-index inclusive), splice '
+            '(either D or A), and strand (either + or -). A header is '
+            'optional, but must start with #.'
+        ),
+    )
+    leg_group.add_argument(
+        '-ss',
+        '--splicing-span',
+        type=int,
+        default=4,
+        help='The splicing span (used in conjunction with --splicing-file.)',
+    )
+
+    print(parser.parse_args())
+    sys.exit(1)
 
     return parser.parse_args()
