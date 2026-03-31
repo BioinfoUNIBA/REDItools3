600c855b893392351da73abdc7b02448392efae9
ahanden <adam.handen@gmail.com>
2026-03-31 16:09:07 -0500

    Added unittest code for RTFastaFile

--- test/unittest/__main__.py
+++ test/unittest/__main__.py
@@ -1,5 +1,6 @@
 import unittest
 from .region import TestRegion
+from .fasta_file import TestRTFastaFile
 from .region_collection import TestRegionCollection
 from .file_utils import TestFileUtils
 from .compiled_position import TestCompiledPosition
