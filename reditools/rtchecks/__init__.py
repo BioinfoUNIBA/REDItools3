bd95b0f85fc27f532f3256c7f4dc5f62d86e5b1e
ahanden <adam.handen@gmail.com>
2026-04-07 13:31:55 -0500

    Removed check_is_none fro rtchecks init

--- reditools/rtchecks/__init__.py
+++ reditools/rtchecks/__init__.py
@@ -3,7 +3,6 @@ from .check_column_min_edits import check_column_min_edits
 from .check_column_min_length import check_column_min_length
 from .check_column_quality import check_column_quality
 from .check_exclusions import check_exclusions
-from .check_is_none import check_is_none
 from .check_max_alts import check_max_alts
 from .check_target_positions import check_target_positions
 from .rtchecks import RTChecks
@@ -14,7 +13,6 @@ __all__ = (
     'check_column_min_length',
     'check_column_quality',
     'check_exclusions',
-    'check_is_none',
     'check_max_alts',
     'check_target_positions',
     'RTChecks',
