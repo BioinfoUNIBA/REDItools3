5b06675146699a9833e0a042b06626a10f2ad4ea
ahanden <adam.handen@gmail.com>
2026-04-07 15:08:03 -0500

    Changed out match statement for earlier python compatability

--- reditools/__main__.py
+++ reditools/__main__.py
@@ -23,18 +23,17 @@ Run Modes:
 
 
 if __name__ == '__main__':
+    toolkit = {
+        'analyze': analyze,
+        'find-repeat': find_repeats,
+        'index': index,
+        'annotate': annotate,
+    }
     if len(sys.argv) > 1:
         command = sys.argv.pop(1)
-        match command:
-            case 'analyze':
-                analyze.main()
-            case 'find-repeats':
-                find_repeats.main()
-            case 'index':
-                index.main()
-            case 'annotate':
-                annotate.main()
-            case _:
-                usage()
+        if command in toolkit:
+            toolkit[command].main()
+        else:
+            usage()
     else:
         usage()
