abaf9c343c3fd6f2359898dcb6ad844c49334684
ahanden <adam.handen@gmail.com>
2026-04-12 14:42:40 -0500

    Updated unittests for rtresult

--- test/analyze/rtchecks.py
+++ test/analyze/rtchecks.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from tempfile import NamedTemporaryFile
 from reditools.tools.analyze.rtchecks import RTChecks
-from reditools.compiled_position import CompiledPosition
+from reditools.compiled_position import CompiledPosition, RTResult
 from reditools.region import Region
 
 class Namespace:
@@ -23,63 +23,66 @@ class TestRTChecks(unittest.TestCase):
             variants=['all'],
         )
 
+    def run_check(self, rtc):
+        return rtc.check(RTResult(self.bases, '*'))
+
     def test_check_column_edit_frequency(self):
         self.options.min_edits = 1
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='A', strand='*')
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.options.min_edits = 0
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.options.min_edits = 1
         rtc = RTChecks(self.options)
         self.bases.add_base(quality=30, base='T', strand='*')
         self.bases.add_base(quality=30, base='T', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.options.min_edits = 3
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
     def test_check_column_min_edits(self):
         self.options.min_edits_per_nucleotide = 1
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='A', strand='*')
         self.bases.add_base(quality=30, base='A', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.options.min_edits_per_nucleotide = 2
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='T', strand='*')
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='T', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='C', strand='*')
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
     def test_check_min_read_depth(self):
         self.options.min_read_depth = 2
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='C', strand='*')
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='A', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='A', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
     def test_check_exclusions(self):
         with NamedTemporaryFile(delete=False, suffix='.bed', mode='w+') as stream:
@@ -89,38 +92,38 @@ class TestRTChecks(unittest.TestCase):
         self.options.exclude_regions = [bed_file]
 
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         with open(bed_file, mode='a') as stream:
             stream.write('chr1\t0\t10\n')
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         with open(bed_file, mode='a') as stream:
             stream.write('chr2\t0\t10\n')
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         os.remove(bed_file)
 
     def test_check_max_editing_nucleotides(self):
         self.options.max_editing_nucleotides = 1
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='A', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='T', strand='*')
         self.bases.add_base(quality=30, base='T', strand='*')
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         self.bases.add_base(quality=30, base='C', strand='*')
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         self.options.max_editing_nucleotides = 2
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
     def test_check_target_positions(self):
         with NamedTemporaryFile(delete=False, suffix='.bed', mode='w+') as stream:
@@ -128,16 +131,16 @@ class TestRTChecks(unittest.TestCase):
             bed_file = stream.name
         self.options.bed_file = [bed_file]
         rtc = RTChecks(self.options)
-        self.assertIsNotNone(rtc.check(self.bases))
+        self.assertIsNotNone(self.run_check(rtc))
 
         with open(bed_file, mode='a') as stream:
             stream.write('chr1\t0\t20\n')
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         with open(bed_file, mode='a') as stream:
             stream.write('chr2\t0\t20\n')
         rtc = RTChecks(self.options)
-        self.assertIsNone(rtc.check(self.bases))
+        self.assertIsNone(self.run_check(rtc))
 
         os.remove(bed_file)
--- test/compiled_position.py
+++ test/compiled_position.py
@@ -1,6 +1,5 @@
 import unittest
-from reditools.compiled_position import CompiledPosition
-
+from reditools.compiled_position import CompiledPosition, RTResult
 
 class TestCompiledPosition(unittest.TestCase):
     def setUp(self):
@@ -12,55 +11,29 @@ class TestCompiledPosition(unittest.TestCase):
         self.cp.add_base(30, '+', 'G')
         self.assertEqual(len(self.cp), 3)
 
-    def test_get_base_counts(self):
-        self.cp.add_base(40, '+', 'A')
-        self.cp.add_base(35, '-', 'A')
-        self.cp.add_base(30, '+', 'C')
-        self.assertEqual(self.cp['A'], 2)
-        self.assertEqual(self.cp['C'], 1)
-        self.assertEqual(self.cp['REF'], 2)
-
-    def test_iter(self):
-        self.cp.add_base(41, '+', 'A')
-        self.cp.add_base(42, '+', 'C')
-        self.cp.add_base(43, '+', 'G')
-        counts = list(self.cp)
-        self.assertEqual(counts, [1, 1, 1, 0])
-
     def test_complement(self):
         self.cp.add_base(11, '+', 'A')
         self.cp.add_base(12, '-', 'C')
         self.cp.complement()
         self.assertEqual(self.cp.bases, ['T', 'G'])
-        self.assertEqual(self.cp.reference, 'T')
-
-    def test_alts_and_variants(self):
-        self.cp.add_base(10, '+', 'C')
-        self.cp.add_base(10, '+', 'A')
-        self.assertEqual(self.cp.alts, ['C'])
-        self.assertEqual(self.cp.variants, ['AC'])
+        self.assertEqual(self.cp.ref, 'T')
 
     def test_calculate_strand(self):
         self.cp.add_base(20, '+', 'A')
         self.cp.add_base(21, '-', 'A')
         self.cp.add_base(22, '+', 'C')
-        with self.assertRaises(ValueError):
-            self.cp.strand
         self.assertEqual(self.cp.calculate_strand(), '+')
-        self.assertEqual(self.cp.strand, '+')
         self.assertEqual(self.cp.calculate_strand(0.7), '*')
-        self.assertEqual(self.cp.strand, '*')
 
     def test_filter_by_strand(self):
         self.cp.add_base(5, '+', 'A')
         self.cp.add_base(5, '+', 'A')
         self.cp.add_base(6, '-', 'C')
-        self.cp.calculate_strand()
-        self.cp.filter_by_strand()
+        self.cp.filter_by_strand('+')
         self.assertEqual(len(self.cp), 2)
-        self.assertEqual(self.cp['A'], 2)
-        self.assertEqual(self.cp['C'], 0)
-        self.assertEqual(self.cp.strand, '+')
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(rtresult['A'], 2)
+        self.assertEqual(rtresult['C'], 0)
 
     def test_filter_by_strand_star(self):
         self.cp.add_base(5, '*', 'A')
@@ -71,15 +44,42 @@ class TestCompiledPosition(unittest.TestCase):
             self.cp.calculate_strand(threshold=1),
             '*',
         )
-        self.cp.filter_by_strand()
-        self.assertEqual(len(self.cp), 4)
-        self.assertEqual(self.cp['A'], 2)
-        self.assertEqual(self.cp['C'], 1)
+        self.cp.filter_by_strand('*')
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(len(rtresult), 4)
+        self.assertEqual(rtresult['A'], 2)
+        self.assertEqual(rtresult['C'], 1)
 
     def test_reference(self):
-        self.assertEqual(self.cp.reference, 'A')
+        self.assertEqual(self.cp.ref, 'A')
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(rtresult.reference, 'A')
 
     def test_len(self):
         self.assertEqual(len(self.cp), 0)
         self.cp.add_base(40, '+', 'A')
         self.assertEqual(len(self.cp), 1)
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(len(rtresult), 1)
+
+    def test_get_base_counts(self):
+        self.cp.add_base(40, '+', 'A')
+        self.cp.add_base(35, '-', 'A')
+        self.cp.add_base(30, '+', 'C')
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(rtresult['A'], 2)
+        self.assertEqual(rtresult['C'], 1)
+        self.assertEqual(rtresult['REF'], 2)
+
+    def test_iter(self):
+        self.cp.add_base(41, '+', 'A')
+        self.cp.add_base(42, '+', 'C')
+        self.cp.add_base(43, '+', 'G')
+        counts = list(RTResult(self.cp, '*'))
+        self.assertEqual(counts, [1, 1, 1, 0])
+
+    def test_variants(self):
+        self.cp.add_base(10, '+', 'C')
+        self.cp.add_base(10, '+', 'A')
+        rtresult = RTResult(self.cp, '*')
+        self.assertEqual(rtresult.variants, ['AC'])
