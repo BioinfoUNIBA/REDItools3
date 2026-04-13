968a9dcdc65069bec64f73422609c5382c340b81
ahanden <adam.handen@gmail.com>
2026-04-13 01:46:35 -0500

    More reditools unittests

--- test/reditools.py
+++ test/reditools.py
@@ -14,6 +14,23 @@ class TestREDItools(unittest.TestCase):
     def setUp(self):
         self.rtools = REDItools()
 
+        self.bam_file = ntf(suffix='.bam')
+        self.fa_file = ntf(suffix='.fa')
+
+        self.sam_obj = SAM()
+        self.sam_obj.add_contig('chr1', length=10)
+        self.sam_obj.add_read('chr1', Sequence(self.sam_obj.genome['chr1'], 0))
+
+        self.sam_obj.genome.save_to_fasta(self.fa_file)
+        self.sam_obj.save_to_sam(self.bam_file, self.fa_file)
+
+        self.rtam = AlignmentManager()
+        self.rtam.add_file(self.bam_file)
+
+    def tearDown(self):
+        os.remove(self.bam_file)
+        os.remove(self.fa_file)
+
     def test_process_bases(self):
         cp = CompiledPosition(ref='A', position=1, contig='chr1')
         cp.add_base(30, '-', 'A')
@@ -42,43 +59,40 @@ class TestREDItools(unittest.TestCase):
         self.assertEqual(rtresult.variants, [])
 
     def test_add_reference(self):
-        bam_file = ntf(suffix='.bam')
-        fa_file = ntf(suffix='.fa')
-
-        sam_obj = SAM()
-        sam_obj.add_contig('chr1', length=5)
-        sam_obj.add_read('chr1', Sequence(sam_obj.genome['chr1'], 0))
-
-        sam_obj.genome.save_to_fasta(fa_file)
-        sam_obj.save_to_sam(bam_file, fa_file)
-
-        rtam = AlignmentManager()
-        rtam.add_file(bam_file)
-
         rtresult = next(
-            self.rtools.analyze(rtam, Region.from_string('chr1', bam_file)),
+            self.rtools.analyze(
+                self.rtam,
+                Region.from_string('chr1', self.bam_file),
+            ),
         )
-        self.assertEqual(rtresult.reference, sam_obj.genome['chr1'][0])
+        self.assertEqual(rtresult.reference, self.sam_obj.genome['chr1'][0])
 
         new_genome = Genome()
         new_genome.add_contig(
             'chr1',
             sequence=''.join(
-                self.complement[_] for _ in sam_obj.genome['chr1']
+                self.complement[_] for _ in self.sam_obj.genome['chr1']
             ),
         )
-        new_genome.save_to_fasta(fa_file)
+        new_genome.save_to_fasta(self.fa_file)
 
         rtresult = next(
-            self.rtools.analyze(rtam, Region.from_string('chr1', bam_file)),
+            self.rtools.analyze(
+                self.rtam,
+                Region.from_string('chr1', self.bam_file),
+            ),
         )
-        self.assertEqual(rtresult.reference, sam_obj.genome['chr1'][0])
+        self.assertEqual(rtresult.reference, self.sam_obj.genome['chr1'][0])
 
-        os.remove(bam_file)
-        os.remove(fa_file)
+    def test_region(self):
+        rtresults = list(self.rtools.analyze(
+            self.rtam,
+            Region.from_string('chr1:3-7', self.bam_file),
+        ))
+        self.assertEqual(len(rtresults), 5)
 
-'''
-    def __init__(self):
-    def analyze(self, alignment_manager, region):
-    def add_reference(self, reference_fname):
-'''
+        rtresults = list(self.rtools.analyze(
+            self.rtam,
+            Region.from_string('chr1:8-20', self.bam_file),
+        ))
+        self.assertEqual(len(rtresults), 3)
